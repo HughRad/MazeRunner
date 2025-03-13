27af99fb03ff2a8f93044d9fcb66dc043821534f
HughRad <145746920+HughRad@users.noreply.github.com>
2025-03-13 13:46:48 +1100

    Delete build directory
    clean up

--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;geometry_msgs;moveit_core;moveit_ros_planning_interface".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "maze_runner"
-PROJECT_SPACE_DIR = "/home/corso/catkin_ws/src/maze_runner/build/devel"
-PROJECT_VERSION = "0.0.1"
